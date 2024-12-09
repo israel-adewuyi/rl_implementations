0fe8fbb2ff636b983da3e501f701f527eaccb15a
Israel Adewuyi <isistickz@gmail.com>
2024-12-09 23:21:08 +0300

    implemented random agent and reward averaging agent

--- multi_armed_bandit/src/Agent.py
+++ multi_armed_bandit/src/Agent.py
@@ -21,4 +21,40 @@ class Agent:
 
     def reset(self, seed: int) -> None:
         self.rng = np.random.default_rng(seed)
+
+class RandomAgent(Agent):
+    """ Random Agent in the MultiArmedBandit environment
+
+        Takes action randomly
+    """
+    def get_action(self) -> ActType:
+        return self.rng.integers(0, self.num_arms)
+
+    def observe(self, action: ActType, reward: float, info: dict) -> int:
+        return 0
+
+class EpsGreedyAgent(Agent):
+    """ The action-value method for this agent is epsilon-greedy i.e 
+            the agent chooses a random action with probability epsilon and 
+            chooses greedy action the rest of the time
+    """
+    def __init__(self, num_arms: int, seed: int, epsilon: float, optimism: float):
+        self.epsilon = epsilon
+        self.optimism = optimism
+        super().__init__(num_arms, seed)
+        
+    def get_action(self) -> ActType:
+        if self.rng.random() < epsilon:
+            return self.rng.integers(0, self.num_arms)
+        else:
+            return int(np.argmax(self.Q))
+
+    def observe(self, action: ActType, reward: float, info: dict):
+        self.N[action] += 1
+        self.Q[action] += (reward - self.Q[action]) / self.N[action]
+
+    def reset(self, seed: int):
+        super().reset(seed=seed)
+        self.Q = np.fill(self.num_arms, self.optimism)
+        self.N = np.zeros(self.num_arms)
         
